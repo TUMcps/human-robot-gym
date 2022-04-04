d0b5961a52497c5911c7cabda50b63c8512d10f0
Jakob Thumm <jakob.thumm@in.tum.de>
2022-04-04 11:41:56 +0200

    Wrote unit test for schunk robot and adapted config files

--- human_robot_gym/controllers/failsafe_controller/safety_shield/tests/bindings/test_safety_shield.py
+++ human_robot_gym/controllers/failsafe_controller/safety_shield/tests/bindings/test_safety_shield.py
@@ -11,9 +11,29 @@ class TestSafetyShield:
 
     shield = SafetyShield(
      activate_shield = True,
-     sample_time = 0.001,
+     sample_time = 0.004,
      trajectory_config_file = dir_path + "/../../config/trajectory_parameters_modrob1.yaml",
      robot_config_file = dir_path + "/../../config/robot_parameters_modrob1.yaml",
+     mocap_config_file = dir_path + "/../../config/cmu_mocap_no_hand.yaml",
+     init_x = 0.0,
+     init_y = 0.0,
+     init_z = 0.0,
+     init_roll = 0.0,
+     init_pitch = 0.0,
+     init_yaw = 0.0
+    )
+    return shield
+
+  @pytest.fixture
+  def shield_schunk(self):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+
+    shield = SafetyShield(
+     activate_shield = True,
+     sample_time = 0.004,
+     trajectory_config_file = dir_path + "/../../config/trajectory_parameters_schunk.yaml",
+     robot_config_file = dir_path + "/../../config/robot_parameters_schunk.yaml",
      mocap_config_file = dir_path + "/../../config/cmu_mocap_no_hand.yaml",
      init_x = 0.0,
      init_y = 0.0,
@@ -111,5 +131,67 @@ class TestSafetyShield:
     assert(abs(robot_caps[5][5] - 1.223754351487657) <= eps)
     assert(abs(robot_caps[5][6] - 0.046806563054797) <= eps)
     
-    
+  # ---------------- Schunk -------------------
+  def test_getRobotReachCapsulesSchunk(self, shield_schunk):
+    motion = Motion(6)
+    shield_schunk.newLongTermTrajectory(motion)
+    dummy_meas = []
+    for i in range(21):
+      dummy_meas.append([0.0, 0.0, 0.0])
+    shield_schunk.humanMeasurement(dummy_meas, 0.0)
+    shield_schunk.step(0.004)
+    robot_caps = shield_schunk.getRobotReachCapsules()
+    for cap in robot_caps:
+      assert(len(cap) == 7)
+      print("-----")
+      print(cap[0])
+      print(cap[1])
+      print(cap[2])
+      print(cap[3])
+      print(cap[4])
+      print(cap[5])
+      print(cap[6])
+    eps = 1e-5
+    assert(abs(robot_caps[0][0] - 0) <= eps)
+    assert(abs(robot_caps[0][1] - -0.015000000000000) <= eps)
+    assert(abs(robot_caps[0][2] - 0.101300000000000) <= eps)
+    assert(abs(robot_caps[0][3] - 0) <= eps)
+    assert(abs(robot_caps[0][4] - -0.015000000000000) <= eps)
+    assert(abs(robot_caps[0][5] - 0.101300000000000) <= eps)
+    assert(abs(robot_caps[0][6] - 0.090000000000000) <= eps)
+    assert(abs(robot_caps[1][0] - 2.295154367360221e-04) <= eps)
+    assert(abs(robot_caps[1][1] - 0.114880476266314) <= eps)
+    assert(abs(robot_caps[1][2] - 0.451203783848448) <= eps)
+    assert(abs(robot_caps[1][3] - -6.617154367385000e-04) <= eps)
+    assert(abs(robot_caps[1][4] - 0.114044823733687) <= eps)
+    assert(abs(robot_caps[1][5] - 0.101205916151551) <= eps)
+    assert(abs(robot_caps[1][6] - 0.074020642029512) <= eps)
+    assert(abs(robot_caps[2][0] - -1.594314036332950e-15) <= eps)
+    assert(abs(robot_caps[2][1] - 1.110223024625157e-15) <= eps)
+    assert(abs(robot_caps[2][2] - 0.436300000000000) <= eps)
+    assert(abs(robot_caps[2][3] - -1.594314036332950e-15) <= eps)
+    assert(abs(robot_caps[2][4] - 1.110223024625157e-15) <= eps)
+    assert(abs(robot_caps[2][5] - 0.436300000000000) <= eps)
+    assert(abs(robot_caps[2][6] - 0.090000000000000) <= eps)
+    assert(abs(robot_caps[3][0] - -4.693429213306548e-05) <= eps)
+    assert(abs(robot_caps[3][1] - -0.005883360941466) <= eps)
+    assert(abs(robot_caps[3][2] - 0.552104849281791) <= eps)
+    assert(abs(robot_caps[3][3] - -2.365707868427609e-06) <= eps)
+    assert(abs(robot_caps[3][4] - 0.061878260941472) <= eps)
+    assert(abs(robot_caps[3][5] - 0.750871950718208) <= eps)
+    assert(abs(robot_caps[3][6] - 0.068496335436544) <= eps)
+    assert(abs(robot_caps[4][0] - -1.545847699099969e-15) <= eps)
+    assert(abs(robot_caps[4][1] - 3.205768983605140e-15) <= eps)
+    assert(abs(robot_caps[4][2] - 0.737500000000000) <= eps)
+    assert(abs(robot_caps[4][3] - -1.545847699099969e-15) <= eps)
+    assert(abs(robot_caps[4][4] - 3.205768983605140e-15) <= eps)
+    assert(abs(robot_caps[4][5] - 0.737500000000000) <= eps)
+    assert(abs(robot_caps[4][6] - 0.070000000000000) <= eps)
+    assert(abs(robot_caps[5][0] - 4.932409872672667e-04) <= eps)
+    assert(abs(robot_caps[5][1] - 0.003349675805265) <= eps)
+    assert(abs(robot_caps[5][2] - 0.851303127433903) <= eps)
+    assert(abs(robot_caps[5][3] - -4.926409872713459e-04) <= eps)
+    assert(abs(robot_caps[5][4] - 0.003181224194743) <= eps)
+    assert(abs(robot_caps[5][5] - 0.931296875069497) <= eps)
+    assert(abs(robot_caps[5][6] - 0.072548874802886) <= eps)    
 
