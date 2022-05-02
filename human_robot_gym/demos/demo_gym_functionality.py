b290f7f8a12e777beb0aec1d7a2f7740df1e0f48
Jakob Thumm <jakob.thumm@in.tum.de>
2022-05-02 16:06:54 +0200

    Added docstrings to demo folder

--- human_robot_gym/demos/demo_gym_functionality.py
+++ human_robot_gym/demos/demo_gym_functionality.py
@@ -1,34 +1,6 @@
-"""
-This script shows how to adapt an environment to be compatible
-with the OpenAI Gym-style API. This is useful when using
-learning pipelines that require supporting these APIs.
+"""This script shows an example of a Panda robot in an human environment.
 
-For instance, this can be used with OpenAI Baselines
-(https://github.com/openai/baselines) to train agents
-with RL.
-
-
-We base this script off of some code snippets found
-in the "Getting Started with Gym" section of the OpenAI
-gym documentation.
-
-The following snippet was used to demo basic functionality.
-
-    import gym
-    env = gym.make('CartPole-v0')
-    for i_episode in range(20):
-        observation = env.reset()
-        for t in range(100):
-            env.render()
-            print(observation)
-            action = env.action_space.sample()
-            observation, reward, done, info = env.step(action)
-            if done:
-                print("Episode finished after {} timesteps".format(t+1))
-                break
-
-To adapt our APIs to be compatible with OpenAI Gym's style, this script
-demonstrates how this can be easily achieved by using the GymWrapper.
+For instance, this can be used with our provided training function to train an RL agent in an human environment.
 """
 
 import robosuite as suite
@@ -39,7 +11,6 @@ import human_robot_gym.environments.manipulation.reach_human_env  # noqa: F401
 
 
 if __name__ == "__main__":
-
     # Notice how the environment is wrapped by the wrapper
     env = GymWrapper(
         suite.make(
