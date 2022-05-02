d28f49aa485b8f9e43bed7ff06389702ac0db2fb
Jakob Thumm <jakob.thumm@in.tum.de>
2022-05-02 19:14:23 +0200

    Finished docstring for environments.

--- human_robot_gym/environments/__init__.py
+++ human_robot_gym/environments/__init__.py
@@ -1,3 +1,4 @@
+"""This package defines all environments and necessary gym wrappers."""
 from robosuite.environments.base import REGISTERED_ENVS, MujocoEnv  # noqa: F401
 
 ALL_ENVIRONMENTS = REGISTERED_ENVS.keys()
