be632764f06ce8bdee4aec6193a0b70f19fb603a
Jakob Thumm <jakob.thumm@in.tum.de>
2022-05-13 12:10:39 -0700

    DinD image is now running. Moved docker build command to the schedule and changed the dummy task

--- gitlab_runner_dind.sh
+++ gitlab_runner_dind.sh
@@ -1,6 +1,13 @@
 docker run -d --name human-robot-gym-gitlab-runner-dind --restart always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /home/thummj/.gitlab-runner:/etc/gitlab-runner \
-    gitlab/gitlab-runner:latest
+  -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  gitlab/gitlab-runner:latest
 
-docker run --rm -it -v /home/thummj/.gitlab-runner:/etc/gitlab-runner gitlab/gitlab-runner:latest register
+docker run --rm -it -v /srv/gitlab-runner/config:/etc/gitlab-runner gitlab/gitlab-runner register -n \
+  --url https://gitlab.lrz.de \
+  --registration-token $GITLABTOKEN \
+  --executor docker \
+  --description "DinD runner" \
+  --docker-image "docker:19.03.12" \
+  --docker-privileged \
+  --docker-volumes "/certs/client"
