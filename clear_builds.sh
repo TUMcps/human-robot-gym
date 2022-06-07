8dacf059b7b31da9e18e4966bff3d5d625dd6af9
Jakob Thumm <jakob.thumm@in.tum.de>
2022-06-07 15:13:03 +0200

    Added a script to delete build files

---
+++ clear_builds.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# This file clears all build files.
+
+rm -rf build
+rm -rf human_robot_gym.egg-info/
+rm -rf human_robot_gym/controllers/failsafe_controller/failsafe_controller.egg-info/
+rm -rf human_robot_gym/controllers/failsafe_controller/build
+rm -rf human_robot_gym/controllers/failsafe_controller/safety_shield/build
