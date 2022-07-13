dd4f76ab31c7ddd55b25463b525a87e202009fb6
jballetshofer <ge96fec@mytum.de>
2022-07-13 15:17:30 +0000

    animation name fix + collision stop rm + new setup

--- human_robot_gym/environments/manipulation/reach_human_env.py
+++ human_robot_gym/environments/manipulation/reach_human_env.py
@@ -6,9 +6,13 @@ Owner:
     Jakob Thumm (JT)
 
 Contributors:
-
+    Julian Balletshofer JB
 Changelog:
     2.5.22 JT Formatted docstrings
+    13.7.22 JB adjusted animation names
+    13.7.22 JB adjusted gym setup to be similar to real
+    13.7.22 JB removed simulation stop when collision
+
 """
 from human_robot_gym.environments.manipulation.human_env import HumanEnv
 
@@ -194,7 +198,7 @@ class ReachHuman(HumanEnv):
         controller_configs=None,
         gripper_types="default",
         initialization_noise="default",
-        table_full_size=(0.4, 0.8, 0.05),
+        table_full_size=(2.5, 0.85, 0.05),
         table_friction=(1.0, 5e-3, 1e-4),
         use_camera_obs=True,
         use_object_obs=True,
@@ -229,7 +233,7 @@ class ReachHuman(HumanEnv):
         human_animation_names=[
             "62_01",
             "62_03",
-            "62_03",
+            "62_04",
             "62_07",
             "62_09",
             "62_10",
@@ -254,7 +258,7 @@ class ReachHuman(HumanEnv):
         self.table_full_size = table_full_size
         self.table_friction = table_friction
         # settings for table top (hardcoded since it's not an essential part of the environment)
-        self.table_offset = np.array((0.0, 0.0, 0.8))
+        self.table_offset = np.array((0.0, -0.48, 0.82))
         # reward configuration
         self.reward_scale = reward_scale
         self.reward_shaping = reward_shaping
@@ -417,8 +421,8 @@ class ReachHuman(HumanEnv):
         Returns:
             done
         """
-        done = super()._check_done(achieved_goal, desired_goal, info)
-        return done or (self._check_success(achieved_goal, desired_goal))
+        # done = super()._check_done(achieved_goal, desired_goal, info)
+        return self._check_success(achieved_goal, desired_goal)
 
     def _get_achieved_goal_from_obs(
         self, observation: Union[List[float], Dict]
