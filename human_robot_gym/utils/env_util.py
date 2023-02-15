943a2677411585e36f3aa923b6656cddbe637065
Felix Trost <ga94zux@mytum.de>
2023-02-15 14:25:57 +0100

    Added expert observation wrapper

--- human_robot_gym/utils/env_util.py
+++ human_robot_gym/utils/env_util.py
@@ -1,4 +1,11 @@
-"""This file describes functions for creating the human-robot-gym environments."""
+"""This file describes functions for creating the human-robot-gym environments.
+
+Contributors:
+    Felix Trost (FT)
+
+Changelog:
+    15.02.23 FT added support for expert observation environments
+"""
 import struct
 from typing import Optional, Dict, Any, Type, Callable, Union, List
 from functools import partial
@@ -9,6 +16,7 @@ from robosuite.wrappers.gym_wrapper import GymWrapper
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecEnv
 from stable_baselines3.common.env_util import make_vec_env as sb3_make_vec_env
 from human_robot_gym.wrappers.goal_env_wrapper import GoalEnvironmentGymWrapper
+from human_robot_gym.wrappers.expert_obs_wrapper import ExpertObsWrapper
 
 
 def make_robosuite_env(
@@ -58,10 +66,26 @@ def make_goal_env(
     return env
 
 
+def make_expert_obs_env(
+    env_id: str,
+    env_kwargs: Optional[Dict[str, Any]] = None,
+    obs_keys: Optional[List[str]] = None,
+    expert_obs_keys: Optional[List[str]] = None,
+) -> gym.Env:
+    """Make the expert obs environment and add the optional TimeLimit wrapper.
+
+    We add the TimeLimit wrapper here because it would require a second Monitor wrapper later.
+    """
+    env = ExpertObsWrapper(make_robosuite_env(env_id, env_kwargs), agent_keys=obs_keys, expert_keys=expert_obs_keys)
+    env = add_time_limit(env, max_episode_steps=env_kwargs.get("horizon", None))
+    return env
+
+
 def make_vec_env(
     env_id: str,
     type: str = "env",
     obs_keys: Optional[List[str]] = None,
+    expert_obs_keys: Optional[List[str]] = None,
     n_envs: int = 1,
     seed: Optional[int] = None,
     start_index: int = 0,
@@ -81,6 +105,7 @@ def make_vec_env(
     :param env_id: the environment ID or the environment class
     :param type: The type of environment to create. Can be "env" or "goal_env".
     :param obs_keys: The observation keys to use for the environment.
+    :param expert_obs_keys: If set, add observations for an expert policy to the info dictionary
     :param n_envs: the number of environments you wish to have in parallel
     :param seed: the initial seed for the random number generator
     :param start_index: start rank index
@@ -99,7 +124,15 @@ def make_vec_env(
     """
     assert type in ["env", "goal_env"], "The type of environment must be either 'env' or 'goal_env'."
     if type == "env":
-        env_callable = partial(make_gym_env, env_id, env_kwargs, obs_keys=obs_keys)
+        if expert_obs_keys is None:
+            env_callable = partial(make_gym_env, env_id, env_kwargs, obs_keys=obs_keys)
+        else:
+            env_callable = partial(
+                make_expert_obs_env,
+                env_id, env_kwargs,
+                obs_keys=obs_keys,
+                expert_obs_keys=expert_obs_keys
+            )
     else:
         env_callable = partial(make_goal_env, env_id, env_kwargs, obs_keys=obs_keys)
     return sb3_make_vec_env(
