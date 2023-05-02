914f1ccd7ce2392e33ab0f9fa64e98234a34f600
Felix Trost <ga94zux@mytum.de>
2023-05-02 16:23:16 +0200

    added demo file

--- human_robot_gym/environments/__init__.py
+++ human_robot_gym/environments/__init__.py
@@ -5,5 +5,8 @@ from human_robot_gym.environments.manipulation.human_env import HumanEnv  # noqa
 from human_robot_gym.environments.manipulation.reach_human_env import ReachHuman  # noqa: F401
 from human_robot_gym.environments.manipulation.reach_human_cartesian_env import ReachHumanCart  # noqa: F401
 from human_robot_gym.environments.manipulation.pick_place_human_cartesian_env import PickPlaceHumanCart  # noqa: F401
+from human_robot_gym.environments.manipulation.pick_place_close_human_cartesian_env import (  # noqa: F401
+    PickPlaceCloseHumanCart
+)
 
 ALL_ENVIRONMENTS = REGISTERED_ENVS.keys()
