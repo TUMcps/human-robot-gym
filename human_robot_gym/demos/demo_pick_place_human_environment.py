c3f5f1ca76d3a755dac2024274cd924d01ae1c87
Felix Trost <ga94zux@mytum.de>
2023-07-20 20:08:06 +0200

    Added possibility to keep the object grabbed when the object is at the target

--- human_robot_gym/demos/demo_pick_place_human_environment.py
+++ human_robot_gym/demos/demo_pick_place_human_environment.py
@@ -175,6 +175,7 @@ if __name__ == "__main__":
         observation_space=env.observation_space,
         action_space=env.action_space,
         signal_to_noise_ratio=0.98,
+        release_when_delivered=False,
     )
 
     use_kb_agent = False
